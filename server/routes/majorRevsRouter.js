dce262b521c796b2204959d724268b90b79261d8
dleonsilva7226 <dleonsilva7226@gmail.com>
2024-11-28 12:39:12 -0500

    Connect data to a specific route in the server localhost

--- server/routes/majorRevsRouter.js
+++ server/routes/majorRevsRouter.js
@@ -1,2 +1,21 @@
 // Have router here and export it here once done
 
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import revData from '../data/majorRevsData.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).json(revData)
+})
+
+router.get('/:majorRevId', (req, res) => {
+    res.status(200).send(path.resolve(__dirname, '../../client/public/majorRev.html'))
+})
+
+export default router
--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 import express from 'express'
+import revRouter from './routes/majorRevsRouter.js'
 
 // Creates app
 const app = express()
+
+//Middleware configuration
 
 //Sends files from public directory
 app.use('/public', express.static('../client/public'))
 
 //Allows client to use files in client/scripts directory
 app.use('/scripts', express.static('../client/public/scripts'))
+
+//Sends majorRev Data from ../data/majorRevsData.js to the following link in the first parameter below
+app.use('/majorRevData', revRouter)
 
 //Gets root route and sends the content in the function to the root route of the website
 app.get('/', (req, res) => {
